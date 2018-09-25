7fd488d94c3a368f07fa2ef9887c758289e3f156
yang <yang.yang@trendy-global.com>
2018-09-25 10:03:19 +0800

    mod: dto required

--- query_service/query_api/crm/entity/dto/crm_daily_report_req_dto.py
+++ query_service/query_api/crm/entity/dto/crm_daily_report_req_dto.py
@@ -3,9 +3,9 @@ from query_service.query_web.controller.crm_controller import ns_1
 
 
 crm_daily_report_req_dto_model = ns_1.model('CrmDailyReportReqDtoModel', {
-    'sales_areas': fields.List(fields.String(description="大区", example='全国')),
-    'cities': fields.List(fields.String(description="城市", example='广州市')),
-    'store_codes': fields.List(fields.String(description="门店编码", example='1102')),
+    'sales_areas': fields.List(fields.String(description="大区", example='全国'), required=True),
+    'cities': fields.List(fields.String(description="城市", example='广州市'), required=True),
+    'store_codes': fields.List(fields.String(description="门店编码", example='1102'), required=True),
     'start_date': fields.Date(required=True, description="开始日期(yyyy-mm-dd)", example='2018-01-01'),
     'end_date': fields.Date(required=True, description="结束日期(yyyy-mm-dd)", example='2018-08-10')
 })
--- query_service/query_api/crm/entity/dto/crm_member_analyse_req_dto.py
+++ query_service/query_api/crm/entity/dto/crm_member_analyse_req_dto.py
@@ -3,11 +3,11 @@ from query_service.query_web.controller.crm_controller import ns_2
 
 
 crm_member_analyse_req_dto_model = ns_2.model('CrmMemberAnalyseReqDtoModel', {
-    'sales_modes': fields.List(fields.String(description="门店类别", example='正价'), required=True),
-    'store_types': fields.List(fields.String(description="门店类型", example='MALL'), required=True),
-    'store_levels': fields.List(fields.String(description="门店等级", example='A'), required=True),
-    'channel_types': fields.List(fields.String(description="经营方式/渠道", example='自营'), required=True),
-    'order_channels': fields.List(fields.String(description="订单类型(线上/线下)", example='线下'), required=True),
+    'sales_modes': fields.List(fields.String(required=True, description="门店类别", example='正价'), required=True),
+    'store_types': fields.List(fields.String(required=True, description="门店类型", example='MALL'), required=True),
+    'store_levels': fields.List(fields.String(required=True, description="门店等级", example='A'), required=True),
+    'channel_types': fields.List(fields.String(required=True, description="经营方式/渠道", example='自营'), required=True),
+    'order_channels': fields.List(fields.String(required=True, description="订单类型(线上/线下)", example='线下'), required=True),
     'country': fields.List(fields.String(description="全国", example='全国')),
     'sales_areas': fields.List(fields.String(description="大区", example='华东')),
     'sales_districts': fields.List(fields.String(description="片区", example='暂无数据')),
