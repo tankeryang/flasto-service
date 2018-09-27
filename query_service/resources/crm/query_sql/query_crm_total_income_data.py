a9f5d17592cda0d6fda037baf8082cc1ce18e68a
yang <yang.yang@trendy-global.com>
2018-09-27 21:08:51 +0800

    mod: sql and param

--- query_service/resources/crm/query_sql/query_crm_total_income_data.py
+++ query_service/resources/crm/query_sql/query_crm_total_income_data.py
@@ -1,7 +1,6 @@
 SQL_CRM_TOTAL_INCOME_REPORT_DATA = """
     WITH tt AS (
-        SELECT {zone}, order_channel, sales_mode, store_type, store_level, channel_type,
-            cast(sum(sales_income) AS DECIMAL(18, 3)) AS sales_income
+        SELECT {zone}, cast(sum(sales_income) AS DECIMAL(18, 3)) AS sales_income
         FROM ads_crm.member_analyse_fold_daily_income_detail
         WHERE member_type = '整体' AND member_newold_type IS NULL AND member_level_type IS NULL
         AND {zone} IN ({zones})
@@ -12,10 +11,9 @@ SQL_CRM_TOTAL_INCOME_REPORT_DATA = """
         AND channel_type IN ({channel_types})
         AND date <= date('{end_date}')
         AND date >= date('{start_date}')
-        GROUP BY {zone}, order_channel, sales_mode, store_type, store_level, channel_type
+        GROUP BY {zone}
     ), lyst AS (
-        SELECT {zone}, order_channel, sales_mode, store_type, store_level, channel_type, member_type,
-            cast(sum(sales_income) AS DECIMAL(18, 3)) AS sales_income
+        SELECT {zone}, member_type, cast(sum(sales_income) AS DECIMAL(18, 3)) AS sales_income
         FROM ads_crm.member_analyse_fold_daily_income_detail
         WHERE member_type IS NOT NULL AND member_newold_type IS NULL AND member_level_type IS NULL
         AND {zone} IN ({zones})
@@ -26,25 +24,22 @@ SQL_CRM_TOTAL_INCOME_REPORT_DATA = """
         AND channel_type IN ({channel_types})
         AND date <= date(date('{end_date}') - interval '1' year)
         AND date >= date(date('{start_date}') - interval '1' year)
-        GROUP BY {zone}, order_channel, sales_mode, store_type, store_level, channel_type, member_type
+        GROUP BY {zone}, member_type
     )
     SELECT DISTINCT
         f.member_type AS member_type,
-        cast(sum(f.sales_income) AS DECIMAL(18, 2)) AS sales_income,
-        cast(COALESCE(TRY(sum(f.sales_income) / tt.sales_income), 0) AS DECIMAL(18, 3)) AS sales_income_proportion,
-        cast(cardinality(array_distinct(flatten(array_agg(f.customer_array)))) AS INTEGER) AS customer_amount,
-        cast(sum(f.order_amount) AS INTEGER) AS order_amount,
-        cast(COALESCE(TRY(sum(f.order_amount) / cardinality(array_distinct(flatten(array_agg(f.customer_array))))), 0) AS INTEGER) AS consumption_frequency,
-        cast(COALESCE(TRY(sum(f.sales_income) / sum(f.order_amount)), 0) AS DECIMAL(18, 2)) AS sales_income_per_order,
-        cast(COALESCE(TRY(sum(f.sales_income) / sum(f.sales_item_quantity)), 0) AS DECIMAL(18, 2)) AS sales_income_per_item,
-        cast(COALESCE(TRY(sum(f.sales_item_quantity) / sum(f.order_amount)), 0) AS DECIMAL(18, 2)) AS sales_item_per_order,
-        cast(COALESCE(TRY(sum(f.sales_income) / lyst.sales_income), 0) AS DECIMAL(18, 4)) AS compared_with_lyst
+        sum(f.sales_income) AS sales_income,
+        COALESCE(TRY(sum(f.sales_income) / tt.sales_income), 0) AS sales_income_proportion,
+        cardinality(array_distinct(flatten(array_agg(f.customer_array)))) AS customer_amount,
+        sum(f.order_amount) AS order_amount,
+        COALESCE(TRY(sum(f.order_amount) / cardinality(array_distinct(flatten(array_agg(f.customer_array))))), 0) AS consumption_frequency,
+        COALESCE(TRY(sum(f.sales_income) / sum(f.order_amount)), 0) AS sales_income_per_order,
+        COALESCE(TRY(sum(f.sales_income) / sum(f.sales_item_quantity)), 0) AS sales_income_per_item,
+        COALESCE(TRY(sum(f.sales_item_quantity) / sum(f.order_amount)), 0) AS sales_item_per_order,
+        COALESCE(TRY(sum(f.sales_income) / lyst.sales_income), 0) AS compared_with_lyst
     FROM ads_crm.member_analyse_fold_daily_income_detail f
-    LEFT JOIN tt ON f.{zone} = tt.{zone} AND f.order_channel = tt.order_channel AND f.sales_mode = tt.sales_mode
-    AND f.store_type = tt.store_type AND f.store_level = tt.store_level AND f.channel_type = tt.channel_type
-    LEFT JOIN lyst ON f.{zone} = lyst.{zone} AND f.order_channel = lyst.order_channel
-    AND f.sales_mode = lyst.sales_mode AND f.store_type = lyst.store_type AND f.store_level = lyst.store_level
-    AND f.channel_type = lyst.channel_type AND f.member_type = lyst.member_type
+    LEFT JOIN tt ON f.{zone} = tt.{zone}
+    LEFT JOIN lyst ON f.{zone} = lyst.{zone} AND f.member_type = lyst.member_type
     WHERE f.member_type IS NOT NULL AND f.member_newold_type IS NULL AND f.member_level_type IS NULL
     AND f.{zone} IN ({zones})
     AND f.order_channel IN ({order_channels})
