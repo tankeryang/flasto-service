cf8266376047a1d1a07151079540df3959b8e265
yang <yang.yang@trendy-global.com>
2018-11-02 15:16:15 +0800

    mod: 优化会员计数详情接口

--- query_service/resources/crm/query_sql/asset/member/zone/sql.py
+++ query_service/resources/crm/query_sql/asset/member/zone/sql.py
@@ -1,29 +1,30 @@
 ALL = """
     WITH tt AS (
-        SELECT brand_name, {zone},
-        array_distinct(flatten(array_agg(register_member_array))) AS register_member_array,
-        cardinality(array_distinct(flatten(array_agg(register_member_array)))) AS register_member_amount
-        FROM ads_crm.member_register_detail
+        SELECT DISTINCT brand_name, {zone}, member_no
+        FROM cdm_crm.member_info_detail
         WHERE brand_name IN ({brands})
         AND {zone} IN ({zones})
         AND sales_mode IN ({sales_modes})
         AND store_type IN ({store_types})
         AND store_level IN ({store_levels})
         AND channel_type IN ({channel_types})
-        AND date <= date('{end_date}') - interval '1' day
-        GROUP BY brand_name, {zone}
+        AND date(member_register_time) <= date('{end_date}') - interval '1' day
+    ), tt_num AS (
+        SELECT brand_name, {zone}, count(DISTINCT member_no) AS register_member_amount
+        FROM tt GROUP BY brand_name, {zone}
     )
     SELECT DISTINCT
         f.brand_name AS brand,
         f.{zone} AS zone,
-        cast(tt.register_member_amount AS INTEGER) AS register_member_amount,
-        cast(cardinality(array_intersect(tt.register_member_array, array_distinct(flatten(array_agg(f.customer_array))))) AS INTEGER) AS consumed_member_amount,
-        cast(cardinality(array_intersect(tt.register_member_array, array_distinct(flatten(array_agg(f.customer_array))))) * 1.0000 / tt.register_member_amount AS DECIMAL(18, 4)) AS consumed_member_amount_proportion,
-        cast(tt.register_member_amount - cardinality(array_intersect(tt.register_member_array, array_distinct(flatten(array_agg(f.customer_array))))) AS INTEGER) AS unconsumed_member_amount,
-        cast(1.0000 - (cardinality(array_intersect(tt.register_member_array, array_distinct(flatten(array_agg(f.customer_array))))) * 1.0000 / tt.register_member_amount) AS DECIMAL(18, 4)) AS unconsumed_member_amount_proportion
-    FROM ads_crm.member_analyse_fold_daily_income_detail f
-    LEFT JOIN tt ON f.brand_name = tt.brand_name AND f.{zone} = tt.{zone}
-    WHERE f.member_type = '会员' AND f.member_newold_type IS NULL AND f.member_level_type IS NULL
+        cast(tt_num.register_member_amount AS INTEGER) AS register_member_amount,
+        cast(count(DISTINCT f.member_no) AS INTEGER) AS consumed_member_amount,
+        cast(count(DISTINCT f.member_no) * 1.0000 / tt_num.register_member_amount AS DECIMAL(18, 4)) AS consumed_member_amount_proportion,
+        cast(tt_num.register_member_amount - count(DISTINCT f.member_no) AS INTEGER) AS unconsumed_member_amount,
+        cast(1.0000 - (count(DISTINCT f.member_no) * 1.0000 / tt_num.register_member_amount) AS DECIMAL(18, 4)) AS unconsumed_member_amount_proportion
+    FROM cdm_crm.order_info_detail f
+    INNER JOIN tt ON f.brand_name = tt.brand_name AND f.{zone} = tt.{zone} AND f.member_no = tt.member_no
+    INNER JOIN tt_num ON f.brand_name = tt_num.brand_name AND f.{zone} = tt_num.{zone}
+    WHERE f.member_type = '会员'
     AND f.brand_name IN ({brands})
     AND f.order_channel IN ({order_channels})
     AND f.{zone} IN ({zones})
@@ -31,8 +32,8 @@ ALL = """
     AND f.store_type IN ({store_types})
     AND f.store_level IN ({store_levels})
     AND f.channel_type IN ({channel_types})
-    AND f.date <= date('{end_date}') - interval '1' day
-    GROUP BY f.brand_name, f.{zone}, tt.register_member_amount, tt.register_member_array
+    AND date(f.order_deal_time) <= date('{end_date}') - interval '1' day
+    GROUP BY f.brand_name, f.{zone}, tt_num.register_member_amount
 """
 
 ########################################################################################################################
