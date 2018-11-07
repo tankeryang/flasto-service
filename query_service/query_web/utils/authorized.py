6248c18cbc9ee135e6d88851d9d2a884f63a96b2
yang <yang.yang@trendy-global.com>
2018-11-08 01:39:46 +0800

    add: 添加token验证

--- query_service/query_web/utils/authorized.py
+++ query_service/query_web/utils/authorized.py
@@ -6,16 +6,14 @@ def authorized(func):
     """验证Header的token"""
     @wraps(func)
     def decorator(*args, **kwargs):
-        token = None
-        
         if 'X-API-KEY' in request.headers:
             token = request.headers['X-API-KEY']
             
             if token == 'Trendy_Crm123':
                 return func(*args, **kwargs)
             else:
-                return dict(message="Token wrong!"), 401
+                return dict(message="Token wrong!", success=False), 401
         else:
-            return dict(message="Token is missing!"), 401
+            return dict(message="Token is missing!", success=False), 401
         
     return decorator
