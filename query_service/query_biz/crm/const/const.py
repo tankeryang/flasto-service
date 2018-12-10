8307537f2e4858603319a11ce094beb40747221f
yang <yang.yang@trendy-global.com>
2018-12-10 15:46:16 +0800

    mod: const 全部

--- query_service/query_biz/crm/const/const.py
+++ query_service/query_biz/crm/const/const.py
@@ -24,8 +24,8 @@ class QueryType:
 class QueryField:
     """查询范围常量"""
     ALL = '全部'
-    ORDER_CHANNELS = {'', '线上', '线下'}
-    SALES_MODES = {'', '正价', '长特', '短特'}
-    STORE_TYPES = {'', 'MALL', '百货', '专卖店'}
-    STORE_LEVELS = {'', 'I', 'A', 'B', 'C', 'D'}
-    CHANNEL_TYPES = {'', '自营', '联营', '特许'}
+    ORDER_CHANNELS = {'线上', '线下'}
+    SALES_MODES = {'正价', '长特', '短特'}
+    STORE_TYPES = {'MALL', '百货', '专卖店'}
+    STORE_LEVELS = {'I', 'A', 'B', 'C', 'D'}
+    CHANNEL_TYPES = {'自营', '联营', '特许'}
