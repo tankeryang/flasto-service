7f8906df945f1dfbd5811f107570ba02047dae74
yang <yang.yang@trendy-global.com>
2019-01-28 20:38:04 +0800

    mod; xlsx

--- query_service/query_biz/crm/service/impl/member_coupon_order_service_impl.py
+++ query_service/query_biz/crm/service/impl/member_coupon_order_service_impl.py
@@ -1,7 +1,7 @@
 import datetime
 import pandas as pd
 from io import BytesIO
-from flask import send_file
+from flask import send_file, make_response, send_from_directory
 
 from query_service.query_api.crm.service.member_coupon_order_service import MemberCouponOrderService
 from query_service.query_biz.crm import const
@@ -53,22 +53,23 @@ class MemberCouponOrderServiceImpl(MemberCouponOrderService):
         df_result.columns = const.MemberCouponOrder.DF_RESULT_COLUMNS
         
         now = datetime.datetime.now().strftime('%Y%m%d_%T:%f')
-        # path = const.ExportFilePath.PATH
+        path = const.ExportFilePath.PATH
         filename = const.MemberCouponOrder.CSV_FILE_NAME + now + '.xlsx'
 
-        output = BytesIO()
+        # output = BytesIO()
         
         # writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        df_result.to_excel(output, index=False, encoding='utf_8_sig', engine='xlsxwriter')
+        df_result.to_excel(path=path + filename, index=False, encoding='utf_8_sig', engine='xlsxwriter')
         # writer.close()
         
-        output.seek(0)
+        # output.seek(0)
         
-        # response = make_response(send_from_directory(path, filename, as_attachment=True))
-        # # response.headers['Content-Type'] = 'text/csv'
-        # response.headers['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+        response = make_response(send_from_directory(path, filename, as_attachment=True))
+        response.headers['Content-Type'] = 'application/octet-stream'
+        response.headers['Content-Disposition'] = 'attachment; filename={}'.format(filename)
         
-        return send_file(output, attachment_filename=filename, as_attachment=True)
+        # return send_file(output, attachment_filename=filename, as_attachment=True)
+        return response
 
     @classmethod
     def get_coupon_denomination_sum(cls, dto):
