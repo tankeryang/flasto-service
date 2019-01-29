2673fc394ff2e22760a3831f910b30d4df112d2d
yang <yang.yang@trendy-global.com>
2019-01-29 15:11:08 +0800

    add: 定时器删除tmp下的excel

--- query_service/query_app.py
+++ query_service/query_app.py
@@ -1,8 +1,28 @@
+import os
+import shutil
+from apscheduler.schedulers.background import BackgroundScheduler
+from query_service.query_biz.crm.const import const
 from query_service.query_web import create_app
 
 
-app = create_app('development')
+PATH = const.ExportFilePath.PATH
+
+
+def job():
+    if os.path.exists(PATH):
+        shutil.rmtree(PATH)
+        os.mkdir(PATH)
+        print('rm success')
+    else:
+        os.mkdir(PATH)
+        print('mkdir success')
+
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0")
+    app = create_app('development')
+    
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(job, trigger='cron', hour='0', minute='0', second='0')
+    
+    scheduler.start()
     app.run()
