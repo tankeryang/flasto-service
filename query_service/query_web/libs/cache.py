11620301643a7d5b6f4e9d15434b5995485f9e08
yang <yang.yang@trendy-global.com>
2019-02-27 20:36:37 +0800

    mod: redis config

--- query_service/query_web/libs/cache.py
+++ query_service/query_web/libs/cache.py
@@ -3,7 +3,7 @@ from flask_caching import Cache
 
 cache = Cache(config=dict(
     CACHE_TYPE='redis',
-    CACHE_REDIS_HOST='10.4.21.175',
-    CACHE_REDIS_PORT=18879,
+    CACHE_REDIS_HOST='redis',
+    CACHE_REDIS_PORT=6379,
     CACHE_REDIS_DB=2
 ))
