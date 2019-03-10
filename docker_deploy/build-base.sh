6507f5c94040069f0c324b5782fb8ee630643e8a
yang <yang.yang@trendy-global.com>
2019-03-10 19:47:00 +0800

    mod: build-base

--- docker_deploy/build-base.sh
+++ docker_deploy/build-base.sh
@@ -2,4 +2,4 @@
 echo "create docker network erm_gw_service..." \
 && docker network create --subnet=192.16.253.1/24 erm_gw_service \
 && echo "build docker image flasto-service-base..." \
-&& docker build -f ./Dockerfile -t flasto-service-base:latest
+&& docker build -t flasto-service-base:latest ./Dockerfile
